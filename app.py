e38e611bb3469f1e00b72602ecfddff0a02214a4
Time-Travller <travllertime1@gmail.com>
2026-07-04 20:03:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from datetime import datetime
 print("Python artifact demo")
 
 with open("result.txt", "w") as file:
-    file.write("Hello from GitHub\n")
-    file.write("This file is created in GitHub Actions\n")
-    file.write(f"Generated Time: {datetime.now()}\n")
+    file.write("i love you prajakta\n")
+    file.write("lvkr firayala jau\n")
+    file.write(udya ya time la: {datetime.now()}\n")
 
 print("result.txt file is created successfully")
