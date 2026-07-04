5804db7e03f689aec8c155fad73fd034410b9aaf
Time-Travller <travllertime1@gmail.com>
2026-07-04 20:04:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from datetime import datetime
 print("Python artifact demo")
 
 with open("result.txt", "w") as file:
-    file.write("i love you prajakta\n")
-    file.write("lvkr firayala jau\n")
-    file.write(udya ya time la: {datetime.now()}\n")
+    file.write("I love you Prajakta\n")
+    file.write("Lavkar firayala jau.\n")
+    file.write(f"Udya ya time la: {datetime.now()}\n")
 
 print("result.txt file is created successfully")
